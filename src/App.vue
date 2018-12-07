845a770f7b7f43c4b8bdba8a179df9942acc6b07
Boris IVANOVIC <ivanovic@d17-l428-pc-07.ipa.iisg.agh.edu.pl>
2018-12-07 09:18:55 +0100

    Friends list

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
-  <div>  
-    <h1>Clicked {{ counter }} times!</h1>
-    <button @click="iAmClicked()">Click me!</button>
+  <div>
+    <div>
+      <h1>Participants list</h1>
+      <ol>
+      	<li v-for="p in participants">{{p.firstname}} {{p.lastname}}</li>
+      </ol>
+    </div>
+    
+    
+
+    <h3>New participant</h3>
+    <form @submit.prevent="addNewParticipant()">
+      <label>Firstname: </label>
+      <input type="text" v-model="firstname">
+      <br />
+      <br />
+      <label>Lastname: </label>
+      <input type="text" v-model="lastname">
+      <br />
+      <br />
+      <button>Add new participant</button>
+    </form>
   </div>
 </template>
- 
+
 <script>
-	
-    export default {
-        data() {
-            return {
-            	counter:0
-            }
-        },
-        
-        methods: {
-            iAmClicked() {
-                this.counter++;
-            }
-        }
-    };
+  export default {
+    data() {
+      return {
+      	
+      		participants: [
+      		{ firstname : 'John', lastname: 'Doe' },
+      		{ firstname : 'Robin', lastname: 'Hood' },
+      		{ firstname : 'Chuck', lastname: 'Norris' },
+      		],
+      		
+      		firstname:'',
+      		lastname:''
+      
+      };
+    },
+    methods: {
+      addNewParticipant() {
+      	
+      		this.participants.push( { firstname : this.firstname, lastname: this.lastname } );
+      		
+      		this.firstname = '';
+      		this.lastname = '';
+      }
+    }
+  };
 </script>
